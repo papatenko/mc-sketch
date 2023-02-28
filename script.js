79dce6d3d61bc862496661542b0f8f7ee966e408
JK-le-dev <jkcondor13@gmail.com>
2023-02-28 16:53:22 -0600

    Color picker has been added
    Color picker has been added, by default the mouse pointer will turn the
    dots of the canvas to a random color; but when selecting a color form
    the color picker, the color of each dot changes to what's within the
    color picker.
    
    Next step is to add a button that changes back to random colors and
    oraganize the code.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Global Variables
+var ColorPicked="rainbow"
+
+// Calling Creaction Functions
+
 createCanvas();
 createButtons();
 
@@ -24,25 +29,41 @@ function createDot() {
     dot.id = "dot";
 
     // Added event listener that would turn each dot into a random color
-    dot.addEventListener("mouseover", () => { dot.style.backgroundColor = colorManager()});
+    dot.addEventListener("mouseover", () => { dot.style.backgroundColor = colorManager(ColorPicked)});
 
     // Add the dot to the page
     document.getElementById("canvas").appendChild(dot);
 }
 
+function createResetButton() {
+    var changeColorButton = document.createElement("button");
+    changeColorButton.innerText = "reset the dang square";
+    changeColorButton.addEventListener("mousedown", () => resetCanvas());
+
+    document.body.appendChild(changeColorButton);
+}
+function createColorPicker() {
+    var colorPicker = document.createElement("input");
+    colorPicker.onchange = "colorSelected(this)";
+    colorPicker.type = "color";
+    colorPicker.id = "colorPicker";
+
+    colorPicker.onchange = event => {ColorPicked = event.target.value} 
+
+    document.body.appendChild(colorPicker);
+}
+
 // Managers
-function colorManager() {
-
-    console.log(document.getElementById("colorPicker").target.value)
-    return document.getElementById("colorPicker").target.value;
-
+function colorManager(colorPicked) {
+    if (colorPicked != "rainbow") {
+        return colorPicked; 
+    }
 
     var red = Math.floor(Math.random() * 255);
     var green = Math.floor(Math.random() * 255);
     var blue = Math.floor(Math.random() * 255);
 
     return "rgb(" + red + "," + green + "," + blue + ")";
-
 }
 
 
@@ -59,31 +80,5 @@ function resetCanvas() {
 }
 
 // Buttons
-function createResetButton() {
-    var changeColorButton = document.createElement("button");
-    changeColorButton.innerText = "reset the dang square";
-    changeColorButton.addEventListener("mousedown", () => resetCanvas());
 
-    document.body.appendChild(changeColorButton);
-}
-function createColorPicker() {
-    var colorPicker = document.createElement("input");
-    colorPicker.onchange = "colorSelected(this)";
-    colorPicker.type = "color";
-    colorPicker.id = "colorPicker";
 
-    // colorPicker.addEventListener("input", updateFirst, false);
-    // colorPicker.addEventListener("change", watchColorPicker, false);
-    colorPicker.onchange = e => {
-
-        console.log(e.target.value)
-    } 
-
-    document.body.appendChild(colorPicker);
-}
-
-function watchColorPicker(event) {
-    document.querySelectorAll("p").forEach((p) => {
-        document.getElementById("header").style.color = event.target.value;
-    });
-}
