4678f443e52af0fddac6f99e0a53009d74d4f7d4
JK-le-dev <jkcondor13@gmail.com>
2023-02-27 20:27:35 -0600

    Color Picker UI has been added
    It doesn't do anything but now there's a button where you can pick a
    color using the built in Color Picker UI.

--- script.js
+++ script.js
@@ -1,65 +1,66 @@
-createCanvas()
-createButtons()
+createCanvas();
+createButtons();
 
-function createCanvas(){
+function createCanvas() {
     for (let index = 0; index < 256; index++) {
-        createDot()
+        createDot();
     }
 }
-function createButtons(){
-    createResetButton()
-    createColorPicker()
+function createButtons() {
+    createResetButton();
+    createColorPicker();
 }
 
-function createDot(){
+function createDot() {
     // Create a new dot element
     var dot = document.createElement("div");
 
     // Set the dot's style properties
-    dot.style.width = "25px"
-    dot.style.height = "25px"
-    dot.style.backgroundColor = "white"
-    dot.id = "dot"
+    dot.style.width = "25px";
+    dot.style.height = "25px";
+    dot.style.backgroundColor = "white";
+    dot.id = "dot";
 
     // Added event listener that would turn each dot into a random color
     dot.addEventListener("mouseover", () => {
-        var red = Math.floor(Math.random() * 255)
-        var green = Math.floor(Math.random() * 255)
-        var blue = Math.floor(Math.random() * 255)
+        var red = Math.floor(Math.random() * 255);
+        var green = Math.floor(Math.random() * 255);
+        var blue = Math.floor(Math.random() * 255);
 
-        dot.style.backgroundColor="rgb("+red+","+green+","+blue+")"
-    })
+        dot.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+    });
 
     // Add the dot to the page
-    document.getElementById('canvas').appendChild(dot);
+    document.getElementById("canvas").appendChild(dot);
 }
-
 // Buttons
 
 function createResetButton() {
-    var changeColorButton = document.createElement("button")
+    var changeColorButton = document.createElement("button");
 
-    changeColorButton.innerText = "reset the dang square"
+    changeColorButton.innerText = "reset the dang square";
 
-    changeColorButton.addEventListener("mousedown", () => resetCanvas())
+    changeColorButton.addEventListener("mousedown", () => resetCanvas());
 
     document.body.appendChild(changeColorButton);
 }
-function createColorPicker(){
+function createColorPicker() {
+    var colorPicker = document.createElement("input");
+    colorPicker.onchange = "colorSelected(this)";
+    colorPicker.type = "color";
 
+    document.body.appendChild(colorPicker);
 }
 
 // Actions
 
-function chooseColor(){
+function chooseColor() {}
 
-}
-
-function resetCanvas(){
-    const canvas = document.querySelector("[id=canvas]")
+function resetCanvas() {
+    const canvas = document.querySelector("[id=canvas]");
 
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
     }
-    createCanvas()
+    createCanvas();
 }
